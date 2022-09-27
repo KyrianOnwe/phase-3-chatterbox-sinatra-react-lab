a1a865771dc4f317ee62dd99ea6391a7ba396aff
Kyrian <dr.kyrian.onwe@gmail.com>
2022-09-27 01:06:17 -0500

    commit tree 35d1a03081c5df82eab894f27d416a9a5869f74f

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,31 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
+  get '/messages' do
+    msgs = Message.all.order(created_at: :asc)
+    msgs.to_json
+  end
+
+  post '/messages' do
+    msg = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+    msg.to_json
+  end
+
+  patch '/messages/:id' do
+    upd = Message.find(params[:id])
+    upd.update(
+      body: params[:body]
+    )
+    upd.to_json
+  end
+
+  delete '/messages/:id' do
+    toss = Message.find(params[:id])
+    toss.destroy
+    toss.to_json
+  end
   
 end
